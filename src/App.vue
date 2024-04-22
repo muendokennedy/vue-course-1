030ae31ad6b50d8ebb3ca257e6f4f7dfeadb9967
muendokennedy <kennedymunyao999@gmail.com>
2024-04-23 00:20:24 +0300

    Initialized a git repository which consists of a vue app and made the Header component

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+import Header from './components/Header.vue';
+</script>
+
+<template>
+    <div class="container">
+      <Header title="Hello Kennedy"/>
+    </div>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Poppins', sans-serif;
+}
+.container{
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+.btn{
+  display: inline-block;
+  border: none;
+  padding: 10px 20px;
+  background: black;
+  color: white;
+  text-decoration: none;
+  border-radius: 5px;
+  margin: 5px;
+  font-size: 15px;
+  cursor: pointer;
+  font-family: inherit;
+}
+.btn:focus{
+  outline: none;
+}
+.btn:active{
+  transform: scale(0.98);
+}
+.btn-block{
+  display: block;
+  width: 100%;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'header',
+  props: {
+    title: String
+  }
+}
+</script>
+
+<style scoped>
+header{
+  display: flex;
+  justify-content: space-between;
+  align-self: center;
+  margin-bottom: 20px;
+}
+</style>
